fecfdda8f41001a9091938afbdb907830423e53b
Hassan <hassan@Hassans-MacBook-Pro.local>
2017-05-09 21:11:56 +0200

    asd

--- core/src/main/scala/org/apache/spark/SparkContext.scala
+++ core/src/main/scala/org/apache/spark/SparkContext.scala
@@ -1955,7 +1955,7 @@ class SparkContext(config: SparkConf) extends Logging {
   		println("Inside Else")
   		val returnRDD= newRdd.map(x=>x*10)
   		rdd.union(returnRDD.asInstanceOf[RDD[T]])
-        println(rdd.first())
+        //println(rdd.first())
         dagScheduler.runJob(rdd, cleanedFunc, Seq.empty[Int], callSite, resultHandler, localProperties.get)
         progressBar.foreach(_.finishAll())
   		rdd.doCheckpoint()
